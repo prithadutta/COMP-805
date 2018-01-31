5d9ace3386001bf9040f05143c745a61b5a96208
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-01-31 00:39:58 +0000

    Create lab2.py

---
+++ labs/week2/lab2.py
@@ -0,0 +1,22 @@
+"""
+lab2.py
+Pritha Dutta
+01/30/2018
+"""
+
+def squared(num_list):
+    """
+    square numbers in num_list
+    num_list:list of numbers
+    Returns:list of these numbers squared
+    """
+    new_list=[]
+    for num in num_list
+       sq_num=pow(num,2):
+       new_list.append(sq_num)
+    return new_list
+
+t1 = [1,2,3]
+res = squared (t1)
+
+#def check_title(title_list):#
