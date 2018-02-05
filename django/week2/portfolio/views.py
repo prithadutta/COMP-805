916446cf4b080ab859bbe81d64207a46f16664a3
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-02-05 22:24:35 +0000

    Update views.py
    commit for week2

--- django/week2/portfolio/views.py
+++ django/week2/portfolio/views.py
@@ -1,22 +1,36 @@
 from django.shortcuts import render
 
 def home(request):
-    '''
+    """
     Renders home page
-    '''
-
-    return render(request,'home.html')
+    """
+    context={}
+    return render(request,'home.html',context)
 
 def resume(request):
+    """
+    Renders resume page
+    """
+    name = "Pritha Dutta"
+    address = "172 Forest Park, Durham, NH-03824"
+    phone = "(603) 285-1724"
+    email = "pd1057@wildcats.unh.edu"
+    skills = ["Java","Python","Django","Software Testing"]
+    context={'my_name':name,'my_addr':address,'my_phone':phone,"my_email":
+	email,"my_skills":skills}
+    return render(request,'resume.html',context)
 
-
-    return render(request, 'resume.html')
+def portfolio(request):
+    """
+    Renders portfolio page
+    """
+    context={}
+    return render(request,'portfolio.html',context)
 
 def contact(request):
-
-
-    return render(request, 'contact.html')
+    """
+    Renders contact page
+    """
+    context={}
+    return render(request,'contact.html',context)
     
-def portfolio(request):
-
-    return render(request, 'portfolio.html')
