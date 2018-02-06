ff11a5056080e19fa8ac03fe24a989f33ed4c837
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-02-06 16:13:50 +0000

    Update urls.py
    commit for week2

--- django/week2/portfolio/urls.py
+++ django/week2/portfolio/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-	  url(r'', views.home, name='home'),
-      url(r'resume', views.resume, name='resume'),
-      url(r'portfolio', views.portfolio, name='portfolio'),
-      url(r'contact', views.contact, name='contact'),
-      url('admin/', admin.site.urls),
+	path(r'', views.home, name='home'),
+	path(r'resume', views.resume, name='resume'),
+	path(r'portfolio', views.portfolio, name='portfolio'),
+	path(r'contact', views.contact, name='contact'),
+    path('admin/', admin.site.urls),
 ]
