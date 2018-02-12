0e96055bc5831dc35da829adef8aa9ea84a19c19
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-02-12 15:42:03 -0600

    Add files via upload

---
+++ django/week3/resume/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Experience, Education
+# Register your models here.
+
+admin.site.register(Experience)
+admin.site.register(Education)
+# Register your models here.
