7e7b0f1e5d67b4b9be9e982b664f6f460e8a0895
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-02-17 21:17:47 -0600

    Add files via upload

---
+++ django/week3/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def home(request):
+	'''
+	Renders home page
+	'''
+	context = {}
+	return render(request,'home.html',context)
+
+def portfolio(request):
+	'''
+	Renders portfolio page
+	'''
+	context = {}
+	return render(request,'portfolio.html',context)
+
+def contact(request):
+	'''
+	Renders contact page
+	'''
+	context = {}
+	return render(request,'contact.html',context)
