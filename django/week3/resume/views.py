4c3978c0015b705eff2d203bec09455677d350d5
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-02-17 20:56:39 -0600

    Add files via upload
    commit for views.py in resume app

---
+++ django/week3/resume/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Education, Experience
+# Create your views here.
+
+def home(request):
+	"""
+	renders the resume home template
+	"""
+	name = "Pritha Dutta"
+	address = "172 Forest Park, Durham, NH-03824"
+	email = "pd1057@wildcats.unh.edu"
+	skills = ["Java","C++", "Python", "Django", "Software Testing"]
+	context = {}
+	educations = Education.objects.order_by('degree')
+	experiences = Experience.objects.order_by('title')
+	context = {'my_education':educations,'my_experiences':experiences,
+	'my_name':name,'my_addr':address,"my_email":email,
+	"my_skills":skills}
+return render(request,'resume/home.html',context)
