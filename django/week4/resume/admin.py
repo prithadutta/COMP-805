c649489a7cb8b8ab387dd6546d8f9e5b56b02d56
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-02-19 17:53:55 +0000

    Update admin.py
    added Resume model in admin.py

--- django/week4/resume/admin.py
+++ django/week4/resume/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Experience, Education
+from .models import Resume, Experience, Education
 # Register your models here.
 
+admin.site.register(Resume)
 admin.site.register(Experience)
 admin.site.register(Education)
