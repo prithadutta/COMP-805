3df1fda0f97d6273c1f874dfc16af40530234cea
pritha dutta <32368861+prithadutta@users.noreply.github.com>
2018-02-19 17:10:54 +0000

    Update tests.py
    tests.py for Resume model

--- django/week4/resume/tests.py
+++ django/week4/resume/tests.py
@@ -8,8 +8,8 @@ class ResumeTest(TestCase):
     resume = Resume(first_name="Pritha", last_name="Dutta")
     education = Education(institution_name = "University of New Hampshire", location = "United States",
         degree = "Master of Science", major = "Information Technology", gpa = 3.78)
-    experience = Experience(title = "Sftware Engineer", location = "India",
-        start_date = 2014-01-17, end_date = 2017-05-31,
+    experience = Experience(title = "Software Engineer", location = "India",
+        start_date = "2014-01-17", end_date = "2017-05-31",
         description = "Software Development")
 
     def setUp(self):
